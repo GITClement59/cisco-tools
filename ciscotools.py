b219bb4b7cc8c6c621dcd4a3462741ae1b7576ad
GITClement59 <51699768+GITClement59@users.noreply.github.com>
2020-04-08 19:06:54 +0200

    Create ciscotools.py

---
+++ ciscotools.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Author : Clément M -GITCLEMENT59
+
+#Version 1.0
+
+#LastUpdate:  08/04/2020
+
+#Récupération de l'user / mdp pour se connecter
+print("Entrez vos informations de connexion: ")
+username = input("Username: \n")
+password = getpass.getpass("Password: \n")
+
+print username
+print password
