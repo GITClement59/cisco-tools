9e2c19e2f8e1245ad67036940be8936b6bf5d67f
GITClement59 <51699768+GITClement59@users.noreply.github.com>
2020-04-09 10:42:31 +0200

    Update ciscotools.py

--- ciscotools.py
+++ ciscotools.py
@@ -16,7 +16,19 @@ print("Entrez vos informations de connexion: ")
 username = input("Username: \n")
 password = getpass.getpass("Password: \n")
 
+def banner():
+    cisco_banner = """
+           _                     _              _     
+      (_)                   | |            | |    
+   ___ _ ___  ___ ___ ______| |_ ___   ___ | |___ 
+  / __| / __|/ __/ _ \______| __/ _ \ / _ \| / __|
+ | (__| \__ \ (_| (_) |     | || (_) | (_) | \__ \
+  \___|_|___/\___\___/       \__\___/ \___/|_|___/
+        """
+    return cisco_banner
+
 def menu():
+    print (banner())
     choice ='0'
     while choice =='0':
         print("CISCO-TOOLS \n Choix disponible 1 à 4. \n")
