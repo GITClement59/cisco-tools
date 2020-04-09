98af6210c826924b662276e892161604b916c2ca
GITClement59 <51699768+GITClement59@users.noreply.github.com>
2020-04-09 14:21:47 +0200

    Update ciscotools.py

--- ciscotools.py
+++ ciscotools.py
@@ -41,57 +41,38 @@ def save_dir():
 #Fonction destiné à sauvegarder les confiugrations des équipements listé dans les deux fichiers
 
 def save():
-    with open('router.list') as f:
-        ip_r = f.read().splitlines()
-    with open('switch.list') as f:
-        ip_s = f.read().splitlines()
-    
-    ip_add = ip_r + ip_s
-    
-    #Boucle sur chaque ligne de la liste ip_add 
-    for ip in ip_add:        
-  
-        equipment = {
-        'device_type': 'cisco_ios',
-        'ip': ip,
-        'username': username,
-        'password': password,
-         }
-        #Créer la connexion sur l'équipement
-        net_connect = ConnectHandler(**equipment)
-        save_dir()
-        #récupération de la running-config
-        run_cnf = net_connect.send_command("show running-config")
-        now = datetime.now()
-        date = now.strftime("%d_%m_%Y")
-        def hostname():
-            sh_hostname = net_connect.send_command("show run | in hostname")
-            hostname = sh_hostname.split()
-            return hostname[1]
-        path_save = "save/{0}/{1}".format(hostname(),date)
-        show_result(path_save)
-        
-        #Ajoute la configuration au fichier texte créer ci dessus
-        with open(path_save, "a") as file:
-            file.write(CONFIGURATION  + "\n")
-            
-#Fonction d'affichage du menu et de sélection         
-def menu():
-    print (banner())
-    choice ='0'
-    while choice =='0':
-        print("\n Choix disponible 1 à 4. \n")
-        print("Choisir '1' pour la configuration avancée. ")
-        print("Choisir '2' pour la sauvegarde intégrale. ")
-        print("Choisir '3' pour l'effacement intégral. ")
-        print("Choisir '4' pour quitter le programme. \n")
-        
-        choice = input ("Sélection: ")
-        
-    if choice == "4":
-        print("\n Fin du programme, merci de votre utilisation.")
-        sys.exit()
-    elif choice == "2":
-        print("lol")
-        save()
+    save_dir()
+    #récupération de la running-config
+    run_cnf = net_connect.send_command("show running-config")
+    now = datetime.now()
+    date = now.strftime("%d_%m_%Y")
+    def hostname():
+        sh_hostname = net_connect.send_command("show run | in hostname")
+        hostname = sh_hostname.split()
+        return hostname[1]
+    path_save = "save/{0}/{1}".format(hostname(),date)
+    show_result(path_save)
+
+    #Ajoute la configuration au fichier texte créer ci dessus
+    with open(path_save, "a") as file:
+        file.write(CONFIGURATION  + "\n")
+
+Affichage du menu 
+def menu(): 
+    print("\n Choix disponible 1 à 4. \n")
+    print("Choisir '1' pour la configuration avancée. ")
+    print("Choisir '2' pour la sauvegarde intégrale. ")
+    print("Choisir '3' pour l'effacement intégral. ")
+    print("Choisir '4' pour quitter le programme. \n")
+
+print (banner())
+menu()
+choice ='0'
+while choice =='0':
+    choice = input ("Sélection: ")
+if choice == "4":
+    print("\n Fin du programme, merci de votre utilisation.")
+    sys.exit()
+elif choice == "2":
+    save()
 menu()
