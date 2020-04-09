e26decf07082deaaa456696f8b4bb761921f9ebe
GITClement59 <51699768+GITClement59@users.noreply.github.com>
2020-04-09 14:05:21 +0200

    Update ciscotools.py

--- ciscotools.py
+++ ciscotools.py
@@ -9,8 +9,9 @@
 
 #importation des modules
 import getpass
+import os
+import datetime
 import sys
-
 #Récupération de l'user / mdp pour se connecter
 print("Entrez vos informations de connexion: ")
 username = input("Username: \n")
@@ -28,6 +29,51 @@ def banner():
     
     return cisco_banner
 
+def hostname():
+    sh_hostname = net_connect.send_command("show run | in hostname")
+    hostname = sh_hostname.split()
+    return hostname[1]
+
+def save_dir():
+    path_save = "save/{0}".format(hostname())
+    rights = 0o755
+    if not os.path.isdir(path_save):
+        os.mkdir(path_save, rights)
+        
+#Fonction destiné à sauvegarder les confiugrations des équipements listé dans les deux fichiers
+
+def save():
+    with open('router.list') as f:
+        ip_r = f.read().splitlines()
+    with open('switch.list') as f:
+        ip_s = f.read().splitlines()
+    
+    ip_add = ip_r + ip_s
+    
+    #Boucle sur chaque ligne de la liste ip_add 
+    for ip in ip_add:        
+  
+        equipment = {
+        'device_type': 'cisco_ios',
+        'ip': ip,
+        'username': username,
+        'password': password,
+         }
+        #Créer la connexion sur l'équipement
+        net_connect = ConnectHandler(**equipment)
+        save_dir()
+        #récupération de la running-config
+        run_cnf = net_connect.send_command("show running-config")
+        now = datetime.now()
+        date = now.strftime("%d_%m_%Y")
+        path_save = "save/{0}/{1}".format(hostname(),date)
+        show_result(path_save)
+        
+        #Ajoute la configuration au fichier texte créer ci dessus
+        with open(path_save, "a") as file:
+            file.write(CONFIGURATION  + "\n")
+            
+#Fonction d'affichage du menu et de sélection         
 def menu():
     print (banner())
     choice ='0'
@@ -43,5 +89,7 @@ def menu():
     if choice == "4":
         print("\n Fin du programme, merci de votre utilisation.")
         sys.exit()
-
-menu()
+    elif choice == "2":
+        print(lol)
+        save()
+  menu()
