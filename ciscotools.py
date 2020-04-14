3466d05c73045d297d8f35fab6c7746c6eeea89c
GITClement59 <51699768+GITClement59@users.noreply.github.com>
2020-04-14 13:09:34 +0200

    Update ciscotools.py

--- ciscotools.py
+++ ciscotools.py
@@ -21,16 +21,16 @@ username = input("Username: \n")
 password = getpass.getpass("Password: \n")
 
 def banner():
-    cisco_banner = """\n
+    print("""\n
        _                     _              _     
       (_)                   | |            | |    
    ___ _ ___  ___ ___ ______| |_ ___   ___ | |___ 
   / __| / __|/ __/ _ \______| __/ _ \ / _ \| / __|
  | (__| \__ \ (_| (_) |     | || (_) | (_) | \__ \
 \n  \___|_|___/\___\___/       \__\___/ \___/|_|___/
-  """
+  """)
     
-    return cisco_banner
+    #return cisco_banner
    
     
 #Fonction qui permet de calculer la durée de la tâche     
